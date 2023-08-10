85029e798af18b22fd54fb2bb16d23d1f0b4b775
AyOne <bettinger.gaspard@gmail.com>
2023-08-10 03:45:36 +0200

    Adding run function to main for testing

---
+++ main.lua
@@ -0,0 +1,17 @@
+main = {}
+
+
+
+
+function main.run()
+	--[[ This is the main function of the plugin. It is called by startup.lua
+	after all the files have been updated. ]]--
+	print("Hello World")
+end
+
+
+
+
+
+
+reutrn main
