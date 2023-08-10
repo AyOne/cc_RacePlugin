ce09ca1b0bb3dffc8dd6fd0d0a93c374b6e8a90f
AyOne <bettinger.gaspard@gmail.com>
2023-08-10 03:42:20 +0200

    fixing variable naming

--- update.lua
+++ update.lua
@@ -15,35 +15,45 @@ local update = {}
 
 -- get the config file and the json script
 function update.get_config()
+	print("Getting the config file from github...")
 	local config = http.get(config_file) or error("Could not get the config file from github")
-	local json = http.get(json_file) or error("Could not get the json script from github")
+	print("Saving the config file...")
 	if config then
 		local file = fs.open("config.json", "w")
 		file.write(config.readAll())
 		file.close()
 		config.close()
 	end
+	print("Config file saved.")
+	print("Getting the json script from github...")
+	local json = http.get(json_file) or error("Could not get the json script from github")
+	print("Saving the json script...")
 	if json then
 		local file = fs.open("json.lua", "w")
 		file.write(json.readAll())
 		file.close()
 		json.close()
 	end
+	print("Json script saved.")
 end
 
 -- get all the files from github
 function update.get_all_files()
+	print("Getting all the files from github...")
 	local config = fs.open("config.json", "r")
 	local json = require("json")
 	local config = json.decode(config.readAll())
-	for i, file in ipairs(config.files) do
-		local file = http.get(github_url..file) or error("Could not get the file from github")
+	for i, fileName in ipairs(config.files) do
+		print("Getting "..fileName.." from github...")
+		local file = http.get(github_url..fileName) or error("Could not get the file from github")
+		print("Saving "..file.."...")
 		if file then
-			local file = fs.open(file, "w")
-			file.write(file.readAll())
-			file.close()
+			local localfile = fs.open(fileName, "w")
+			localfile.write(file.readAll())
+			localfile.close()
 			file.close()
 		end
+		print(fileName.." saved.")
 	end
 end
 
