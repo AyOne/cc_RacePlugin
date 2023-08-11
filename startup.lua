57d57d37f4d23d304c71ffaefde061a02f6560eb
AyOne <bettinger.gaspard@gmail.com>
2023-08-12 01:56:13 +0200

    fixed another error and arg to not update

--- startup.lua
+++ startup.lua
@@ -1,7 +1,10 @@
 -- making sure all the files are up to date
-local update = require("update") or error("Could not load the update script")
-update.get_config()
-update.get_all_files()
+if args[1] == "update" then
+	local update = require("update") or error("Could not load the update script")
+	update.get_config()
+	update.get_all_files()
+	return
+end
 
 -- running the main script
 local main = require("main") or error("Could not load the main script")
