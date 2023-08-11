83dd5a8ef16a478a621eef23518597e0ffb2a4c6
AyOne <bettinger.gaspard@gmail.com>
2023-08-12 01:44:16 +0200

    added most of the logic of the basic race

--- main.lua
+++ main.lua
@@ -8,7 +8,11 @@ config = json.decode(config.readAll())
 
 playerDetector = peripheral.find("playerDetector")
 chatBox = peripheral.find("chatBox")
-
+monitor = peripheral.find("monitor")
+monitor.setTextScale(0.5)
+monitor.clear()
+cur_x, cur_y = 1,1
+monitor.setCursorPos(1,1)
 
 
 racing_player = nil
@@ -33,44 +37,57 @@ function race()
 	racing_player_data = database.get_player(player)
 	chatBox.sendMessageToPlayer("You have subscribe to the race, it will start soon !", racing_player, "Race Plugin")
 	sleep(2)
-	chatBox.sendMessageToPlayer("Be ready on the starting line in 3...", racing_player, "Race Plugin")
-	sleep(1)
-	chatBox.sendMessageToPlayer("2...", racing_player, "Race Plugin")
-	sleep(1)
-	chatBox.sendMessageToPlayer("1...", racing_player, "Race Plugin")
+	chatBox.sendMessageToPlayer("Be ready on the starting line in 10...", racing_player, "Race Plugin")
+	for i=1, 9 do
+		sleep(1)
+		charBox.sendMessageToPlayer(10 - i.."..."m racing_player, "Race Plugin")
+	end
 	sleep(1)
 	chatBox.sendMessageToPlayer("Gooo !!!", racing_player, "Race Plugin")
 
 	local start_time = os.time()
 	local current_time = start_time
 	local current_checkpoint = 0
+	checkpoint_to_reach = build_checkpoint(config["checkpoints"]["start"]["from"], config["checkpoints"]["start"]["to"])
 	while true do
-		checkpoint_to_reach = nil
-		if (current_checkpoint == 0) then
-			racing_player_data["checkpoints"]["start"]
-		elseif (current_checkpoint < #config["checkpoints"]) then
-			checkpoint_to_reach = racing_player_data["checkpoints"][current_checkpoint]
-		else
-			checkpoint_to_reach = racing_player_data["checkpoints"]["finish"]
-		end
-
-		if (checkpoint_to_reach ~= nil) then
-			local pos = playerDetector.getPlayerPos(racing_player)
-			-- check if player pos are in between checkpoint_to_reach.from and checkpoint_to_reach.to
-
-			if (pos.x >= checkpoint_to_reach.from.x and pos.x <= checkpoint_to_reach.to.x) then
-				if (pos.y >= checkpoint_to_reach.from.y and pos.y <= checkpoint_to_reach.to.y) then
-					if (pos.z >= checkpoint_to_reach.from.z and pos.z <= checkpoint_to_reach.to.z) then
-						-- player is in the checkpoint
-						current_checkpoint = current_checkpoint + 1
-						racing_player_data["checkpoints"][current_checkpoint] = current_time - start_time
-						-- database.update_player(racing_player, racing_player_data)
-					end
-				end
+		if (player_in_checkpoint(racing_player, checkpoint_to_reach)) then
+			current_checkpoint = current_checkpoint + 1
+			if (current_checkpoint > #config["checkpoints"]) then
+				checkpoint_to_reach = build_checkpoint(config["checkpoints"]["finish"]["from"], config["checkpoints"]["finish"]["to"])
+			else
+				checkpoint_to_reach = build_checkpoint(config["checkpoints"][current_checkpoint]["from"], config["checkpoints"][current_checkpoint]["to"])
 			end
 		end
 	end
 end
+
+
+function build_checkpoint(from, to)
+	hitbox = {}
+	hitbox["max_x"] = math.max(from.x, to.x)
+	hitbox["min_x"] = math.min(from.x, to.x)
+	hitbox["max_y"] = math.max(from.y, to.y)
+	hitbox["min_y"] = math.min(from.y, to.y)
+	hitbox["max_z"] = math.max(from.z, to.z)
+	hitbox["min_z"] = math.min(from.z, to.z)
+	return hitbox
+end
+
+function player_in_checkpoint(player, checkpoint)
+	local pos = playerDetector.getPlayerPos(player)
+	monitor.write(player.name.." : "..pos.x.." "..pos.y.." "..pos.z)
+	cur_y = cur_y + 1
+	monitor.setCursorPos(1, cur_y)
+	if (pos.x >= checkpoint.from.x and pos.x <= checkpoint.to.x) then
+		if (pos.y >= checkpoint.from.y and pos.y <= checkpoint.to.y) then
+			if (pos.z >= checkpoint.from.z and pos.z <= checkpoint.to.z) then
+				return true
+			end
+		end
+	end
+	return false
+end
+
 
 
 function wait_for_player()
